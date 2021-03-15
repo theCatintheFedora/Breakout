c12e406239da0d29bea448fc399a73913953d2c3
Timothy Price <theCatintheFedora1367@gmail.com>
2021-03-15 11:00:03 -0500

    added brick or "conga line of blue brick, yellow ball, and white rectangle travel through an empty universe"

--- Breakout/GameScene.swift
+++ Breakout/GameScene.swift
@@ -11,6 +11,7 @@ import GameplayKit
 class GameScene: SKScene {
     var ball = SKShapeNode()
     var paddle = SKSpriteNode()
+    var brick = SKSpriteNode()
     
     override func didMove(to view: SKView) {
         createBackground()
@@ -22,6 +23,7 @@ class GameScene: SKScene {
         // this stuff happens before each game starts
         makeBall()
         makePaddle()
+        makeBrick()
     }
     
     func createBackground() {
@@ -75,6 +77,14 @@ class GameScene: SKScene {
         paddle.physicsBody?.isDynamic = false
         addChild(paddle)
     }
+    
+    func makeBrick() {
+        brick.removeFromParent()    // remove the brick, if it exists
+        brick = SKSpriteNode(color: .blue, size: CGSize(width: 50, height: 20))
+        brick.position = CGPoint(x: frame.midX, y: frame.maxY - 50)
+        brick.name = "brick"
+        brick.physicsBody = SKPhysicsBody(rectangleOf: brick.size)
+        brick.physicsBody?.isDynamic = false
+        addChild(brick)
+    }
 }
-
-
