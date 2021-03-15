a88d139fcc153ffb9ff8a6621900b746c36e5fd2
Timothy Price <theCatintheFedora1367@gmail.com>
2021-03-15 10:56:42 -0500

    created paddle object or "yellow ball chased by white rectangle through an empty universe"

--- Breakout/GameScene.swift
+++ Breakout/GameScene.swift
@@ -10,6 +10,7 @@ import GameplayKit
 
 class GameScene: SKScene {
     var ball = SKShapeNode()
+    var paddle = SKSpriteNode()
     
     override func didMove(to view: SKView) {
         createBackground()
@@ -18,9 +19,10 @@ class GameScene: SKScene {
     }
     
     func resetGame() {
-            // this stuff happens before each game starts
-            makeBall()
-        }
+        // this stuff happens before each game starts
+        makeBall()
+        makePaddle()
+    }
     
     func createBackground() {
         let stars = SKTexture(imageNamed: "Stars")
@@ -38,31 +40,41 @@ class GameScene: SKScene {
     }
     
     func makeBall() {
-            ball.removeFromParent()
-                // remove the ball (if it exists)
-            ball = SKShapeNode(circleOfRadius: 10)
-            ball.position = CGPoint(x: frame.midX, y: frame.midY)
-            ball.strokeColor = .black
-            ball.fillColor = .yellow
-            ball.name = "ball"
-                // physics shape matches ball image
-            ball.physicsBody = SKPhysicsBody(circleOfRadius: 10)
-                // ignores all forces and impulses
-            ball.physicsBody?.isDynamic = false
-                // use precise collision detection
-            ball.physicsBody?.usesPreciseCollisionDetection = true
-                // no loss of energy from friction
-            ball.physicsBody?.friction = 0
-                // gravity is not a factor
-            ball.physicsBody?.affectedByGravity = false
-                // bounces fully off of other objects
-            ball.physicsBody?.restitution = 1
-                // does not slow down over time
-            ball.physicsBody?.linearDamping = 0
-            ball.physicsBody?.contactTestBitMask = (ball.physicsBody?.collisionBitMask)!
-            addChild(ball)
-                // add ball object to the view
-        }
+        ball.removeFromParent()
+        // remove the ball (if it exists)
+        ball = SKShapeNode(circleOfRadius: 10)
+        ball.position = CGPoint(x: frame.midX, y: frame.midY)
+        ball.strokeColor = .black
+        ball.fillColor = .yellow
+        ball.name = "ball"
+        // physics shape matches ball image
+        ball.physicsBody = SKPhysicsBody(circleOfRadius: 10)
+        // ignores all forces and impulses
+        ball.physicsBody?.isDynamic = false
+        // use precise collision detection
+        ball.physicsBody?.usesPreciseCollisionDetection = true
+        // no loss of energy from friction
+        ball.physicsBody?.friction = 0
+        // gravity is not a factor
+        ball.physicsBody?.affectedByGravity = false
+        // bounces fully off of other objects
+        ball.physicsBody?.restitution = 1
+        // does not slow down over time
+        ball.physicsBody?.linearDamping = 0
+        ball.physicsBody?.contactTestBitMask = (ball.physicsBody?.collisionBitMask)!
+        addChild(ball)
+        // add ball object to the view
+    }
+    
+    func makePaddle() {
+        paddle.removeFromParent()   // remove the paddle, if it exists
+        paddle = SKSpriteNode(color: .white, size: CGSize(width: frame.width/4, height: 20))
+        paddle.position = CGPoint(x: frame.midX, y: frame.minY + 125)
+        paddle.name = "paddle"
+        paddle.physicsBody = SKPhysicsBody(rectangleOf: paddle.size)
+        paddle.physicsBody?.isDynamic = false
+        addChild(paddle)
+    }
 }
 
 
