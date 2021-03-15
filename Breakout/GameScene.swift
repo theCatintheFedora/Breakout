081ef94927bee412790806ec54dbd608ab9becbd
Timothy Price <theCatintheFedora1367@gmail.com>
2021-03-15 10:54:21 -0500

    created a ball or "lone yellow ball travels through an empty universe on a quest to find his home"

--- Breakout/GameScene.swift
+++ Breakout/GameScene.swift
@@ -9,10 +9,19 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
+    var ball = SKShapeNode()
+    
     override func didMove(to view: SKView) {
         createBackground()
+        resetGame()
         // this stuff happens once (when the app opens)
     }
+    
+    func resetGame() {
+            // this stuff happens before each game starts
+            makeBall()
+        }
+    
     func createBackground() {
         let stars = SKTexture(imageNamed: "Stars")
         for i in 0...1 {
@@ -27,6 +36,33 @@ class GameScene: SKScene {
             starsBackground.run(moveForever)
         }
     }
+    
+    func makeBall() {
+            ball.removeFromParent()
+                // remove the ball (if it exists)
+            ball = SKShapeNode(circleOfRadius: 10)
+            ball.position = CGPoint(x: frame.midX, y: frame.midY)
+            ball.strokeColor = .black
+            ball.fillColor = .yellow
+            ball.name = "ball"
+                // physics shape matches ball image
+            ball.physicsBody = SKPhysicsBody(circleOfRadius: 10)
+                // ignores all forces and impulses
+            ball.physicsBody?.isDynamic = false
+                // use precise collision detection
+            ball.physicsBody?.usesPreciseCollisionDetection = true
+                // no loss of energy from friction
+            ball.physicsBody?.friction = 0
+                // gravity is not a factor
+            ball.physicsBody?.affectedByGravity = false
+                // bounces fully off of other objects
+            ball.physicsBody?.restitution = 1
+                // does not slow down over time
+            ball.physicsBody?.linearDamping = 0
+            ball.physicsBody?.contactTestBitMask = (ball.physicsBody?.collisionBitMask)!
+            addChild(ball)
+                // add ball object to the view
+        }
 }
 
 
