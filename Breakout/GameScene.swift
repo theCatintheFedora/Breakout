c1df738cd7eaebb1263c8529aac2a7cb65b10375
Timothy Price <theCatintheFedora1367@gmail.com>
2021-03-16 10:32:38 -0500

    Determined win conditions

--- Breakout/GameScene.swift
+++ Breakout/GameScene.swift
@@ -38,6 +38,20 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         }
     }
     
+    func didBegin(_ contact: SKPhysicsContact) {
+            if contact.bodyA.node?.name == "brick" ||
+               contact.bodyB.node?.name == "brick" {
+                print("You win!")
+                brick.removeFromParent()
+                ball.removeFromParent()
+            }
+            if contact.bodyA.node?.name == "loseZone" ||
+               contact.bodyB.node?.name == "loseZone" {
+                print("You lose!")
+                ball.removeFromParent()
+            }
+        }
+    
     func resetGame() {
         // this stuff happens before each game starts
         makeBall()
